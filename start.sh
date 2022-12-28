14f5468c8eb74d97e0b157ef105939af43611a5e
Soby <110024336+Soby370@users.noreply.github.com>
2022-12-28 13:48:56 +0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
-git clone https://github.com/LazyDeveloperr/MissPerfect-URL-BOT /MissPerfectURLBOT
-cd /MissPerfectURLBOT
+git clone https://github.com/Your_Repo_Name /LazyDeveloper
+cd /LazyDeveloper
 pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
